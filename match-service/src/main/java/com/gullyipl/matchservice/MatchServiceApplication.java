812e17939c15b507b6ea9048460749967421abef
joystern13 <joystern13@gmail.com>
2019-05-19 10:52:46 +0200

    changes for google cloud

--- match-service/src/main/java/com/gullyipl/matchservice/MatchServiceApplication.java
+++ match-service/src/main/java/com/gullyipl/matchservice/MatchServiceApplication.java
@@ -2,15 +2,13 @@ package com.gullyipl.matchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableRdsInstance(dbInstanceIdentifier="${cloud.aws.rds.dbInstanceIdentifier}", username="${cloud.aws.rds.springaws.username}" , password="${cloud.aws.rds.springaws.password}")
+//@EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.gullyipl.matchservice.repository")
-public class MatchServiceApplication {
+public class MatchServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatchServiceApplication.class, args);
