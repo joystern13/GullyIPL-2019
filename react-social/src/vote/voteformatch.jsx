6695b691ee1c33d8cd87e157ebf5a5e838149721
tejas20k <tjs_klh@yahoo.co.in>
2019-05-27 18:35:26 +0200

    ajax call for getting matches

--- react-social/src/vote/voteformatch.jsx
+++ react-social/src/vote/voteformatch.jsx
@@ -6,6 +6,7 @@ import classNames from "classnames";
 import PropTypes from "prop-types";
 import ReactRadioButtonGroup from "react-radio-button-group";
 import { Typography } from "@material-ui/core";
+import axios from "axios";
 
 // Material helpers
 import { withStyles } from "@material-ui/core";
@@ -27,7 +28,10 @@ class VoteForMatch extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedOption: "option1"
+      selectedOption: "option1",
+      error: null,
+      isLoaded: false,
+      matches: []
     };
   }
 
@@ -44,41 +48,83 @@ class VoteForMatch extends Component {
     console.log("You have submitted:", this.state.selectedOption);
   };
 
+  componentDidMount() {
+    axios
+      .get("http://mapps.cricbuzz.com/cbzios/series/2697/matches")
+      .then(function(response) {
+        console.log(response);
+      });
+    fetch("http://mapps.cricbuzz.com/cbzios/series/2697/matches")
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            matches: result.matches
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
   render() {
     const { classes, className, ...rest } = this.props;
+    const { error, isLoaded, matches } = this.state;
 
     const rootClassName = classNames(classes.root, className);
 
-    return (
-      <div className="container">
-        <form onSubmit={this.handleFormSubmit}>
-          <div className="cc-selector">
-            <Typography variant="body1" className="matchText">
-              1st Warm-up game County Ground Bristol, England, 24 May 2019 10:30
-              AM GMT
-            </Typography>
-            <ReactRadioButtonGroup
-              options={[
-                {
-                  value: "AFG",
-                  label: "AFG",
-                  itemClassName: "team1",
-                  labelClassName: "drinkcard-cc AFG"
-                },
-                {
-                  value: "AUS",
-                  label: "AUS",
-                  itemClassName: "team2",
-                  labelClassName: "drinkcard-cc AUS"
-                }
-              ]}
-              name="rdbTeam"
-              isStateful={true}
-            />
-          </div>
-        </form>
-      </div>
-    );
+    // if (error) {
+    //   return <div>Error: {error.message}</div>;
+    // } else
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="container">
+          <form onSubmit={this.handleFormSubmit}>
+            <div className="cc-selector">
+              <ul>
+                {matches.map(match => (
+                  <li key={match.match_id}>
+                    {match.match_id} === {match.header.match_desc}
+                  </li>
+                ))}
+              </ul>
+              <Typography variant="body1" className="matchText">
+                1st Warm-up game County Ground Bristol, England, 24 May 2019
+                10:30 AM GMT
+              </Typography>
+              <ReactRadioButtonGroup
+                options={[
+                  {
+                    value: "AFG",
+                    label: "AFG",
+                    itemClassName: "team1",
+                    labelClassName: "drinkcard-cc AFG"
+                  },
+                  {
+                    value: "AUS",
+                    label: "AUS",
+                    itemClassName: "team2",
+                    labelClassName: "drinkcard-cc AUS"
+                  }
+                ]}
+                name="rdbTeam"
+                isStateful={true}
+              />
+            </div>
+          </form>
+        </div>
+      );
+    }
   }
 }
 
