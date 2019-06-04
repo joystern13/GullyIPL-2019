825a8e57d75f0ecb1b3dae16d2927953a5c3e802
joystern13 <joystern13@gmail.com>
2019-06-04 18:59:38 +0200

    fill colour corrected

--- auth-service/src/main/java/com/gullycricket/authservice/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ auth-service/src/main/java/com/gullycricket/authservice/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -57,7 +57,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         Optional<String> redirectUri = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
                 .map(Cookie::getValue);
-
+        System.out.println(redirectUri);
         if(redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
             throw new BadRequestException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
         }
