074651e5c8b019359ce6734db218a98ff76559cc
joystern13 <joystern13@gmail.com>
2019-06-17 19:52:25 +0200

    fix for double slash (//)

--- react-social/src/constants/index.js
+++ react-social/src/constants/index.js
@@ -3,14 +3,14 @@
 // export const VOTING_BASE_URL = "http://localhost:8182/gullycricket/votes";
 export const API_BASE_URL = "https://auth-service-dot-gullyipl.appspot.com";
 export const MATCH_BASE_URL =
-  "https://match-service-dot-gullyipl.appspot.com/gullycricket/matches/";
+  "https://match-service-dot-gullyipl.appspot.com/gullycricket/matches";
 export const VOTING_BASE_URL =
   "https://voting-service-dot-gullyipl.appspot.com/gullycricket/votes";
 
 export const ACCESS_TOKEN = "accessToken";
 export const USER_ID = "userId";
 
-//export const OAUTH2_REDIRECT_URI = "http://localhost:3000/oauth2/redirect";
+// export const OAUTH2_REDIRECT_URI = "http://localhost:3000/oauth2/redirect";
 export const OAUTH2_REDIRECT_URI =
   "http://www.gully-cricket.tk/oauth2/redirect";
 
